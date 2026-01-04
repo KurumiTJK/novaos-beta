e57bf7b72777de56c3577150d7d79aae2fb69d95
KurumiSilva <tommyjkang@gmail.com>
2026-01-03 22:20:19 -0800

    v3.7.11-beta

--- novaos-backend/src/tests/e2e/index.ts
+++
@@ -1,3 +0,0 @@
-// E2E Tests Index
-export * from './utils.js';
-export * from './learn-rust-flow.test.js';
--- novaos-backend/src/tests/integration/index.ts
+++
@@ -1,4 +0,0 @@
-// Integration Tests Index
-export * from './auth-flow.test.js';
-export * from './goal-creation.test.js';
-export * from './spark-completion.test.js';
